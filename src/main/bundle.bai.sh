b9abd771ac1223932de6a499862c4d23da68383f
TacB0sS <adam.vdk@nu-art-software.com>
2025-07-29 18:31:34 +0300

    releasing v0.0.8

--- src/main/bundle.bai.sh
+++ src/main/bundle.bai.sh
@@ -2,10 +2,10 @@
 
 ## Bundle: bootstrap
 ## Description: Thunderstorm setup and patch utilities
-source <(curl -fsSL https://github.com/nu-art/bash-tools/releases/latest/download/bundle.loader.sh) -b tools
+source <(curl -fsSL https://github.com/nu-art/bash-tools/releases/latest/download/bundle.loader.sh) -b lib.tools -f
 
 import "./backup.sh"
 import "./build-and-install.sh"
 import "./cli.sh"
 
-bai.run
+bai.run "$@"
