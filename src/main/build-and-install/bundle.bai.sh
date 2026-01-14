717d809ec7b7247c7c71a0405d062a5d326c57f2
TacB0sS <adam.vdk@nu-art-software.com>
2026-01-14 15:25:19 +0200

    use new loader flags

--- src/main/build-and-install/bundle.bai.sh
+++ src/main/build-and-install/bundle.bai.sh
@@ -2,7 +2,7 @@
 
 ## Bundle: bootstrap
 ## Description: Thunderstorm setup and patch utilities
-source <(curl -fsSL https://github.com/nu-art/bash-tools/releases/latest/download/bundle.loader.sh) -b lib.tools -f
+source <(curl -fsSL https://github.com/nu-art/bash-tools/releases/latest/download/bundle.loader.sh) --sh-bundle lib.tools -sh-f
 
 import "./backup.sh"
 import "./build-and-install.sh"
